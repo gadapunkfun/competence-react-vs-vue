bc3ce11d2b2a48ae03f42912311d7f4386b8fd8d
gadapunkfun <robert.matijevic@devies.se>
2022-01-27 00:55:47 +0100

    :factory: moved MutationTypes to seperate file

---
+++ vue/src/models/Store/MutationTypes/TodoMutationTypes.ts
@@ -0,0 +1,6 @@
+export enum TodoMutationTypes {
+	ADD_TODO = "addTodo",
+	REMOVE_TODO = "removeTodo",
+	FINISH_TODO = "finishTodo",
+	UPDATE_TODOS = "updateTodos"
+}
--- vue/src/store/index.ts
+++ vue/src/store/index.ts
@@ -1,16 +1,10 @@
-import { TodoItem } from "@/models/TodoItem";
 import { InjectionKey } from "vue";
 import { createStore, Store } from 'vuex'
+import { TodoItem } from "@/models/TodoItem";
+import { TodoMutationTypes } from "@/models/Store/MutationTypes/TodoMutationTypes";
 
 export interface State {
 	currentTodos: TodoItem[];
-}
-
-export enum MutationTypes {
-	ADD_TODO = "addTodo",
-	REMOVE_TODO = "removeTodo",
-	FINISH_TODO = "finishTodo",
-	UPDATE_TODOS = "updateTodos"
 }
 
 export const key: InjectionKey<Store<State>> = Symbol();
@@ -21,38 +15,38 @@ export const store = createStore<State>({
 	},
 	actions: {
 		addTodo(store, todoItem: TodoItem) {
-			store.commit(MutationTypes.ADD_TODO, todoItem);
+			store.commit(TodoMutationTypes.ADD_TODO, todoItem);
 		},
 		removeTodo(store, todoItem: TodoItem) {
-			store.commit(MutationTypes.REMOVE_TODO, todoItem);
+			store.commit(TodoMutationTypes.REMOVE_TODO, todoItem);
 		},
 		finishTodo(store, todoItem: TodoItem) {
-			store.commit(MutationTypes.FINISH_TODO, todoItem);
+			store.commit(TodoMutationTypes.FINISH_TODO, todoItem);
 		},
 		updateTodos(store) {
-			store.commit(MutationTypes.UPDATE_TODOS);
+			store.commit(TodoMutationTypes.UPDATE_TODOS);
 		}
 	},
 	mutations: {
-		[MutationTypes.ADD_TODO](state, todoItem: TodoItem) {
-			if (todoItem) {
+		[TodoMutationTypes.ADD_TODO](state, todoItem: TodoItem) {
+			if (todoItem && todoItem.content) {
 				state.currentTodos.push(todoItem);
 			}
 		},
-		[MutationTypes.REMOVE_TODO](state, todoItem: TodoItem) {
+		[TodoMutationTypes.REMOVE_TODO](state, todoItem: TodoItem) {
 			const indexOfTodo = state.currentTodos.findIndex(todo => todo == todoItem);
 			if (todoItem && indexOfTodo >= 0) {
 				state.currentTodos = state.currentTodos.filter(todo => todo != todoItem)
 			}
 		},
-		[MutationTypes.FINISH_TODO](state, todoItem: TodoItem) {
+		[TodoMutationTypes.FINISH_TODO](state, todoItem: TodoItem) {
 			const indexOfTodo = state.currentTodos.findIndex(todo => todo == todoItem);
 			if (todoItem && indexOfTodo >= 0) {
 				state.currentTodos[indexOfTodo].isDone = !state.currentTodos[indexOfTodo].isDone;
 				localStorage.setItem("currentTodos", JSON.stringify(state.currentTodos));
 			}
 		},
-		[MutationTypes.UPDATE_TODOS](state: State) {
+		[TodoMutationTypes.UPDATE_TODOS](state: State) {
 			localStorage.setItem("currentTodos", JSON.stringify(state.currentTodos));
 		}
 	}
