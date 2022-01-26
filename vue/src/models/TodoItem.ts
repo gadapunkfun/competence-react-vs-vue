f84fdc84c1f13c051a767f99beae214c95047ac6
gadapunkfun <robert.matijevic@devies.se>
2022-01-27 00:56:28 +0100

    :factory: replace TodoItem interface with a class

--- vue/src/models/TodoItem.ts
+++ vue/src/models/TodoItem.ts
@@ -1,8 +1,16 @@
-import { DateTime } from "luxon";
-
-export interface TodoItem {
+export class TodoItem {
 	content: string;
 	isDone: boolean;
-	created: DateTime;
-	completed?: DateTime;
+	created: string;
+	completed?: string;
+	constructor(content: string) {
+		this.content = content;
+		this.isDone = false;
+		this.created = new Date().toLocaleDateString("ja-JP", {
+			weekday: "long",
+			year: "numeric",
+			month: "short",
+			day: "2-digit"
+		});
+	}
 }
