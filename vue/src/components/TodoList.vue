0b43f892b7f412cb03b590f63459af282b613584
gadapunkfun <robert.matijevic@devies.se>
2022-01-29 18:04:24 +0100

    :factory: changed to grid layout and changed script to JS

--- vue/src/components/TodoList.vue
+++ vue/src/components/TodoList.vue
@@ -1,52 +1,33 @@
 <template>
-	<!-- <div class="container">
-		<div class="todo">
-			<div class="todo--input">
-				<input v-model="todoContent" type="text" name="New todo" />
-				<button @click="addTodo">Add todo</button>
-			</div>
-			<div v-if="todos.length > 0" class="todo--todos">
-				<todo-list-item
-					v-for="(todoItem, index) in todos"
-					:todoItem="todoItem"
-					:key="index"
-				/>
-			</div>
-			<div v-else>
-				<h3>No todos</h3>
-			</div>
-		</div>
-	</div>-->
-	<n-space vertical>
-		<n-input-group>
-			<n-input
-				v-model:value="todoContent"
-				type="text"
-				size="medium"
-				clearable
-				rese
-				@keydown.enter="addTodo"
-				@keyup.enter="todoContent = ''"
-			></n-input>
-			<n-button type="primary" size="medium" @click="addTodo">Add todo</n-button>
-		</n-input-group>
-	</n-space>
-	<n-grid :cols="3">
+    <n-grid :x-gap="10" :y-gap="5" :cols="4">
+        <n-gi span="24">
+            <n-input-group>
+                <n-input
+                    v-model:value="todoContent"
+                    type="text"
+                    size="medium"
+                    clearable
+                    rese
+                    @keydown.enter="addTodo"
+                    @keyup.enter="todoContent = ''"
+                ></n-input>
+                <n-button type="primary" size="medium" @click="addTodo">Add todo</n-button>
+            </n-input-group>
+        </n-gi>
 		<n-gi v-for="(todoItem, index) in todos" :key="index">
 			<todo-list-item :todoItem="todoItem" />
 		</n-gi>
-	</n-grid>
+    </n-grid>    
 </template>
 
-<script lang="ts">
-import { store } from "@/store";
-import { defineComponent } from "vue";
+<script>
+import store from "@/store";
 import { TodoItem } from "@/models/TodoItem";
 import TodoListItem from "@/components/TodoListItem.vue";
-import { TodoMutationTypes } from "@/models/Store/MutationTypes/TodoMutationTypes";
+import { TodoMutationTypes } from "@/models/store/todo/TodoMutationTypes";
 
-export default defineComponent({
-	components: {
+export default {
+    components: {
 		TodoListItem,
 	},
 	data() {
