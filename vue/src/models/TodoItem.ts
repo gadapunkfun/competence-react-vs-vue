7c53a92c37263123db57871085007b07776e7060
gadapunkfun <robert.matijevic@devies.se>
2022-01-29 18:04:59 +0100

    :factory: code cleanup

--- vue/src/models/TodoItem.ts
+++ vue/src/models/TodoItem.ts
@@ -1,16 +1,16 @@
 export class TodoItem {
-	content: string;
-	isDone: boolean;
-	created: string;
-	completed?: string;
-	constructor(content: string) {
-		this.content = content;
-		this.isDone = false;
-		this.created = new Date().toLocaleDateString("ja-JP", {
-			weekday: "long",
-			year: "numeric",
-			month: "short",
-			day: "2-digit"
-		});
-	}
+    content: string;
+    isDone: boolean;
+    created: string;
+    completed?: string;
+    constructor(content: string) {
+        this.content = content;
+        this.isDone = false;
+        this.created = new Date().toLocaleDateString("ja-JP", {
+            weekday: "long",
+            year: "numeric",
+            month: "short",
+            day: "2-digit"
+        });
+    }
 }
