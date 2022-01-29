abd85497deaa95fc21b017b13713107fc99c043b
gadapunkfun <robert.matijevic@devies.se>
2022-01-29 18:03:00 +0100

    :sparkles: added Todo false api

---
+++ vue/src/api/todoApi.ts
@@ -0,0 +1,28 @@
+import { TodoItem } from "@/models/TodoItem";
+
+export const getTodos = async (): Promise<TodoItem[]> => {
+    const todosJson = localStorage.getItem("todos");
+    const todos = todosJson ? (JSON.parse(todosJson) as TodoItem[]) : [];
+    return fakeDelayedResponse(todos, 500);
+};
+
+export const addTodo = async (todo: TodoItem): Promise<TodoItem> => {
+    const todosJson = localStorage.getItem("todos");
+    const todos = todosJson ? (JSON.parse(todosJson) as TodoItem[]) : [];
+
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+    return fakeDelayedResponse(todo, 500);
+};
+
+const fakeDelayedResponse = async <T>(
+    response: T,
+    delayInMsSeconds: number
+): Promise<T> => {
+    return new Promise(function (resolve) {
+    setTimeout(function () {
+        resolve(response);
+    }, delayInMsSeconds);
+    });
+};
