fbee3d5184f1fabf7a34a31d7f9391e85027ef21
gadapunkfun <robert.matijevic@devies.se>
2022-02-17 13:45:06 +0100

    added unique Id for TodoItem

--- vue/src/models/TodoItem.ts
+++ vue/src/models/TodoItem.ts
@@ -1,16 +1,18 @@
 export class TodoItem {
-    content: string;
-    isDone: boolean;
-    created: string;
-    completed?: string;
-    constructor(content: string) {
-        this.content = content;
-        this.isDone = false;
-        this.created = new Date().toLocaleDateString("ja-JP", {
-            weekday: "long",
-            year: "numeric",
-            month: "short",
-            day: "2-digit"
-        });
-    }
+	id: number;
+	content: string;
+	isDone: boolean;
+	created: string;
+	completed?: string;
+	constructor(content: string) {
+		this.id = Math.floor(Math.random() * 100000);
+		this.content = content;
+		this.isDone = false;
+		this.created = new Date().toLocaleDateString("ja-JP", {
+			weekday: "long",
+			year: "numeric",
+			month: "short",
+			day: "2-digit"
+		});
+	}
 }
